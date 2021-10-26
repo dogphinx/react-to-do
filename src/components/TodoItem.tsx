223144cf335a5c5ad022ccbc2f3a725dd908a0e8
Ming <saydits08@gmail.com>
2021-10-26 20:58:12 +0900

    first commit

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+//  투두 하나의 아이템 그거 하나
+
+const TodoItem: FC = () => {
+  return <></>;
+};
+// FC 펑셔널 컴포넌트 타입.
+// NFC 지금은 안쓰는데 props 를 받지않는 정적 함수형 컴포넌트. 여기까지만
+export default TodoItem;
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,9 @@
+//  투두 아이템을 임포트해서 반복해주는 프레젠테이션 컴포넌트
+import { FC } from "react";
+
+const TodoList: FC = () => {
+  return <></>;
+};
+// FC 펑셔널 컴포넌트 타입.
+// NFC 지금은 안쓰는데 props 를 받지않는 정적 함수형 컴포넌트. 여기까지만
+export default TodoList;
